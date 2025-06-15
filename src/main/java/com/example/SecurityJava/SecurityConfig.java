c64f3e7f27616d495d2831c25d68ff6447c37e61
malkeet <malkeet1996singh@gmail.com>
2025-06-15 17:30:30 +0530

    1st commit

--- src/main/java/com/example/SecurityJava/SecurityConfig.java
+++ src/main/java/com/example/SecurityJava/SecurityConfig.java
@@ -2,30 +2,40 @@ package com.example.SecurityJava;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
     //Copied from class SpringBootWebSecurityConfiguration
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated());
         //Make Session stateless
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(csrf -> csrf.disable());
         return http.build();
     }
 
@@ -38,9 +48,16 @@ public class SecurityConfig {
 
             UserDetails admin = User.withUsername("admin")
                         .password("{noop}password1")
-                    .roles("USER")
+                    .roles("ADMIN")
                     .build();
+
+            /*
+            InMemoryUserDetailsManager used to create in memory user db
+            JdbcUserDetailsManager used to create user in DB
+             */
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
 
         return  new InMemoryUserDetailsManager(user1, admin);
     }
+
 }
